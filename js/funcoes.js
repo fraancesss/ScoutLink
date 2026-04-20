11189645dd2343a7ff11e1aaee7100c21ad73a3f
fraancesss <guilhermefranca438@gmail.com>
2026-04-20 17:37:38 -0400

    endereço

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,23 +1,24 @@
  function irCadastrar() {
-      window.location.href = "../../paginas/Cadastro/cadastro.html";
+      window.location.href = "/paginas/Cadastro/cadastro.html";
     }
     function irEntrar() {
-      window.location.href = "../../paginas/Login/login.html";
+      window.location.href = "paginas/Login/login.html";
     }
     function irLoud() {
-      window.location.href = "../../paginas/Organizacao/organizacao.html";
+      window.location.href = "paginas/Organizacoes/organizacoes.html";
     }
     function irFuria() {
-      window.location.href = "../../paginas/Organizacao/organizacao.html";
+      window.location.href = "paginas/Organizacoes/organizacoes.html";
     }
     function irMibr() {
-      window.location.href = "../../paginas/Organizacao/organizacao.html";
+      window.location.href = "/paginas/Organizacoes/organizacoes.html";
     }
     function irJogador() {
-      window.location.href = "../../paginas/Talento/talento.html";
+      window.location.href = "/paginas/Talento/talento.html";
     }
     function irPerfil() {
       window.location.href = "../../paginas";
     }
     function enviarFeedback() {
-  alert("Feedback enviado com sucesso!");    }
+  alert("Feedback enviado com sucesso!");   
+ }
